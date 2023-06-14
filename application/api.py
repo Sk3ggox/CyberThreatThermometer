ba18d705793bda6d1c82f8f937540b82e13bff96
Ryan Waltmans <r.waltmans@student.fontys.nl>
2023-06-14 12:38:09 +0200

    Added CORSMiddleware, created ruleset exclude 3,4

--- application/api.py
+++ application/api.py
@@ -1,8 +1,18 @@
 import re
 from datetime import datetime, timedelta
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/get_alerts")
 async def get_alerts():
