71550e3bd97c56b1cb8c16ffe1cd9388292e0667
Ryan Waltmans <r.waltmans@student.fontys.nl>
2023-06-14 14:04:08 +0200

    no clue if this works

--- application/remove_lines.py
+++ application/remove_lines.py
@@ -4,11 +4,10 @@ from datetime import datetime, timedelta
 
 def remove_lines(file_path, threshold_minutes):
     threshold_time = datetime.now() - timedelta(minutes=threshold_minutes)
+    pattern = r'(\d{2}\/\d{1,2}-\d{2}:\d{2}:\d{2}\.\d{6}).*'
     
-    with fileinput.FileInput(file_path, inplace=True) as file:
+    with open(file_path, "r") as file, open(file_path, "w") as file_write:
         for line in file:
-            timestamp_str = line.split(',')[0].strip()
-            timestamp = datetime.strptime(timestamp_str, "%m/%d-%H:%M:%S.%f")
-
-            if timestamp >= threshold_time:
-                print(line, end='')
+            timestamp = datetime.strptime(str(datetime.now().year) + "/" + re.match(pattern, line).group(1), "%Y/%m/%d-%H:%M:%S.%f").isoformat()
+            if timestamp > threshold_time.isoformat():
+                file_write.write(line)
